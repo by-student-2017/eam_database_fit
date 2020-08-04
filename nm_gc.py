f25b51416fe606debe593a4b91526ae4c25137b5
by-student-2017 <studentsctest@gmail.com>
2020-08-04 18:10:28 +0900

    google colaboratroy

--- nm_gc.py
+++ nm_gc.py
@@ -151,12 +151,12 @@ def f(x):
   rhoe2   = commands.getoutput("cat F.plt | awk '{if($1>"+str(x[2])+"){print $2}}' | head -2 | tail -1")
   rhoout1 = commands.getoutput("cat F.plt | awk '{if($1<"+str(rhoout)+"){print $2}}' | tail -2 | head -1")
   rhoout2 = commands.getoutput("cat F.plt | awk '{if($1>"+str(rhoout)+"){print $2}}' | head -2 | tail -1")
-  print "F near boundary 1, F: "+str(rhoin1)+" : "+str(rhoin2)
-  print "F near boundary 2, F: "+str(rhoe1)+" : "+str(rhoe2)
-  print "F near boundary 3, F: "+str(rhoout1)+" : "+str(rhoout2)
+  print "F near boundary 1, F: "+str(rhoin1)+" : "+str(rhoin2)+" | diff "+str(rhoin1 - rhoin2)
+  print "F near boundary 2, F: "+str(rhoe1)+" : "+str(rhoe2)+" | diff "+str(rhoe1 - rhoe2)
+  print "F near boundary 3, F: "+str(rhoout1)+" : "+str(rhoout2)+" | diff "+str(rhoout1 - rhoout2)
   print "---------------"
 
-  y = abs(diffea)**2 + 1000*abs(float(rhoin1) - float(rhoin2))**2 + 1000*abs(float(rhoe1) - float(rhoe2))**2  + 1000*abs(float(rhoout1) - float(rhoout2))**2
+  y = abs(diffea)**2 + 1000*abs(rhoin1 - rhoin2)**2 + 1000*abs(rhoe1 - rhoe2)**2  + 1000*abs(rhoout1 - rhoout2)**2
 
   print "Evaluate: ", y
   #print "Parameters: ", x
