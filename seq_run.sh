8a77ff607e11436d63653ea8aa63920570b3b6a3
by-student-2017 <studentsctest@gmail.com>
2020-08-05 23:10:24 +0900

    ubuntu 18.04 LTS

--- seq_run.sh
+++ seq_run.sh
@@ -6,9 +6,10 @@ do
   sed -i 's/Xx/'${atom}'/g' EAM.input
   cp in.lmp_temp_v2 in.lmp_temp
   sed -i 's/YYYY/300.0/' in.lmp_temp
-  python nm_gc.py
+  python nm_debian.py
   mv config_potfit config_potfit_${atom}_300K
   mv energy.dat energy_${atom}_300K.dat
+  mv EAM_code EAM_code_${atom}_300K.dat
   echo -n > config_potfit
   echo -n > energy.dat
 done
