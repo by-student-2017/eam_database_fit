50838da8e100d66c15d28b30ef3dd70604b519a2
by-student-2017 <studentsctest@gmail.com>
2020-08-07 11:39:17 +0900

    ubuntu 18.04 LTS

--- baysian_v3_gc.py
+++ baysian_v3_gc.py
@@ -192,7 +192,7 @@ def descripter(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18
     print >> f, text
 
   commands.getoutput("./Zhou04_EAM_3 < EAM.input")
-  if count > 2000 or count % int(200-count/10) == 1: 
+  if count > 2000 or count % int(abs(200-count/10)+1) == 1: 
     commands.getoutput(lammps_adress+" < in.lmp")
     commands.getoutput("cp ./cfg/run.50.cfg run.50.cfg")
     commands.getoutput("./cfg2vasp/cfg2vasp run.50.cfg")
