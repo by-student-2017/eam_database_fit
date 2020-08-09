82cbed24985155950228a43c372e88fabcddb904
by-student-2017 <studentsctest@gmail.com>
2020-08-09 22:03:22 +0900

    ubuntu 18.04 LTS

--- baysian_v3.py
+++ baysian_v3.py
@@ -192,7 +192,7 @@ def descripter(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18
     print >> f, text
 
   commands.getoutput("./Zhou04_EAM_2 < EAM.input")
-  if count > 3000 or count % int(600*2.718(-count/600)+1) == 1: 
+  if count > 3000 or count % int(600*2.718**(-count/600)+1) == 1: 
     commands.getoutput(lammps_adress+" < in.lmp")
     commands.getoutput("cp ./cfg/run.50.cfg run.50.cfg")
     commands.getoutput("./cfg2vasp/cfg2vasp run.50.cfg")
--- nm_v4_debian.py
+++ nm_v4_debian.py
@@ -124,7 +124,7 @@ def f(x):
   for t in temp:
     print "---------------"
     print "Temperature: "+str(t)+" [K]"
-   if count > 3000 or count % int(600*2.718(-count/600)+1) == 1: 
+    if count > 3000 or count % int(600*2.718**(-count/600)+1) == 1: 
       commands.getoutput("mv data.in_"+str(t)+"K data.in")
       natom = commands.getoutput("awk '{if($2==\"atoms\"){print $1}}' data.in")
       commands.getoutput(lammps_adress+" < in.lmp_"+str(t)+"K")
