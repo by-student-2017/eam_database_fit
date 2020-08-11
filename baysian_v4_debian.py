22a749812eb5dcdb8a1dc106860e21968eb3db8c
by-student-2017 <studentsctest@gmail.com>
2020-08-11 14:20:38 +0900

    ubuntu 18.04 LTS

--- baysian_v4_debian.py
+++ baysian_v4_debian.py
@@ -7,14 +7,16 @@ import sys
 file_tmp = 'EAM_code.tmp'
 file_inp = 'EAM_code'
 
-lammps_adress = "lmp"
 cif2cell_adress = "cif2cell"
 
 commands.getoutput("setenv OMP_NUM_THREADS 1")
 num_core = commands.getoutput("grep 'core id' /proc/cpuinfo | sort -u | wc -l")
+#lammps_adress = "mpirun -np "+str(num_core)+" --allow-run-as-root lmp"
 #pwscf_adress = "mpirun -np "+str(num_core)+" --allow-run-as-root pw.x"
-#pwscf_adress = "mpirun -np "+str(num_core)+" pw.x"
-pwscf_adress = "mpirun -np 2 pw.x"
+lammps_adress = "mpirun -np "+str(num_core)+" lmp"
+pwscf_adress = "mpirun -np "+str(num_core)+" pw.x"
+#lammps_adress = "mpirun -np 2 lmp"
+#pwscf_adress = "mpirun -np 2 pw.x"
 
 satom = commands.getoutput("grep \"atomtype\" EAM.input | sed -e \"s/.*=//\" -e \"s/'//g\"")
 
@@ -26,13 +28,24 @@ commands.getoutput("mkdir cfg")
 commands.getoutput("mkdir work")
 commands.getoutput("echo -n > energy.dat")
 
-temp_K = commands.getoutput("awk '{if($2==\"temp\"){print $4}}' in.lmp")
-print "Lammps MD: "+temp_K+" K"
+natom = 5000
+fxl = numpy.ones(int(natom)+1)
+fyl = numpy.ones(int(natom)+1)
+fzl = numpy.ones(int(natom)+1)
+fxp = numpy.ones(int(natom)+1)
+fyp = numpy.ones(int(natom)+1)
+fzp = numpy.ones(int(natom)+1)
 
-target = [0,0,0] # dummy data
-y_str = [0] # dummy data
-natom = commands.getoutput("awk '{if($2==\"atoms\"){print $1}}' data.in")
-
+temp = [77,273,1073,1573]
+for t in temp:
+  commands.getoutput("cp in.lmp in.lmp_"+str(t)+"K")
+  commands.getoutput("sed -i 's/YYYY/"+str(t)+"/' in.lmp_"+str(t)+"K")
+  commands.getoutput("cp data.in data.in_"+str(t)+"K")
+# if you would set other structures, e.g.,
+#commands.getoutput("cp data.in data.in_77K")
+#commands.getoutput("cp data.in data.in_273K")
+#commands.getoutput("cp ./data/data.in.BCC data.in_1073K")
+#commands.getoutput("cp ./data/data.in.HCP data.in_1578K")
 #----------------------------------------------------------------------
 print "read parameters from EAM_code.init"
 nline = commands.getoutput("grep -n "+str(satom)+" EAM_code.init | head -1 | sed -e \"s/:.*//g\"")
@@ -185,87 +198,111 @@ def descripter(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18
   with open(file_inp,'w') as f:
     print >> f, text
 
-  commands.getoutput("./Zhou04_EAM_2 < EAM.input")
-  if count > 3000 or count % int(600*2.718**(-count/600)+1) == 1: 
-    commands.getoutput(lammps_adress+" < in.lmp")
-    commands.getoutput("cp ./cfg/run.50.cfg run.50.cfg")
-    commands.getoutput("./cfg2vasp/cfg2vasp run.50.cfg")
-    commands.getoutput("python ./vasp2cif/vasp2cif.py run.50.vasp")
-    commands.getoutput(cif2cell_adress+" run.50.vasp.cif --no-reduce -p pwscf --pwscf-pseudo-PSLibrary-libdr=\"./potentials\" --setup-all --k-resolution=0.48 --pwscf-force=yes --pwscf-stress=yes --pwscf-run-type=scf -o pw.in") 
-    commands.getoutput(pwscf_adress+" < pw.scf.in")
-    commands.getoutput(cif2cell_adress+" run.50.vasp.cif --no-reduce -p pwscf --pwscf-pseudo-PSLibrary-libdr=\"./potentials\" --setup-all --k-resolution=0.18 --pwscf-force=yes --pwscf-stress=yes --pwscf-run-type=scf -o pw.in") 
-    commands.getoutput(pwscf_adress+" < pw.scf.in > pw.out")
-    commands.getoutput("./pwscf2force >> config_potfit")
-    commands.getoutput(cif2cell_adress+" run.50.vasp.cif --no-reduce -p lammps  -o data_fix.in")
-    commands.getoutput(lammps_adress+" < in.lmp_fix")
-    commands.getoutput("mv data.in.restart data.in")
+  commands.getoutput("./Zhou04_EAM_3 < EAM.input")
+
+  tdiffea = 0.0
+  tdiffp  = 0.0
+  tdifff  = 0.0
+  for t in temp:
+    print "---------------"
+    print "Temperature: "+str(t)+" [K]"
+    if count > 3000 or count % int(600*2.718**(-count/600)+1) == 1: 
+      commands.getoutput("mv data.in_"+str(t)+"K data.in")
+      natom = commands.getoutput("awk '{if($2==\"atoms\"){print $1}}' data.in")
+      commands.getoutput(lammps_adress+" < in.lmp_"+str(t)+"K")
+      commands.getoutput("cp ./cfg/run.50.cfg run.50.cfg")
+      commands.getoutput("./cfg2vasp/cfg2vasp run.50.cfg")
+      commands.getoutput("python ./vasp2cif/vasp2cif.py run.50.vasp")
+      commands.getoutput(cif2cell_adress+" run.50.vasp.cif --no-reduce -p pwscf --pwscf-pseudo-PSLibrary-libdr=\"./potentials\" --setup-all --k-resolution=0.48 --pwscf-force=yes --pwscf-stress=yes --pwscf-run-type=scf -o pw.in") 
+      commands.getoutput(pwscf_adress+" < pw.scf.in")
+      commands.getoutput(cif2cell_adress+" run.50.vasp.cif --no-reduce -p pwscf --pwscf-pseudo-PSLibrary-libdr=\"./potentials\" --setup-all --k-resolution=0.18 --pwscf-force=yes --pwscf-stress=yes --pwscf-run-type=scf -o pw.in") 
+      commands.getoutput(pwscf_adress+" < pw.scf.in > pw.out")
+      commands.getoutput("./pwscf2force >> config_potfit_"+str(satom))
+      commands.getoutput(cif2cell_adress+" run.50.vasp.cif --no-reduce -p lammps -o data_fix.in_"+str(t)+"K")
+      commands.getoutput("cp data_fix.in_"+str(t)+"K data_fix.in")
+      commands.getoutput(lammps_adress+" < in.lmp_fix")
+      commands.getoutput("mv data.in.restart data.in_"+str(t)+"K")
     #
-    commands.getoutput("./pwscf2force > config")
-  else:
-    commands.getoutput(lammps_adress+" < in.lmp_fix")
+      commands.getoutput("./pwscf2force > config_"+str(t)+"K")
+    else:
+      commands.getoutput("cp data_fix.in_"+str(t)+"K data_fix.in")
+      natom = commands.getoutput("awk '{if($2==\"atoms\"){print $1}}' data_fix.in")
+      commands.getoutput(lammps_adress+" < in.lmp_fix")
+    print "number of atoms: "+str(natom)
 
-  # 1 bar = 0.0001 GPa
-  # stress = pressure
-  pxxl = commands.getoutput("awk '{if($1==\"pxxl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
-  pyyl = commands.getoutput("awk '{if($1==\"pyyl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
-  pzzl = commands.getoutput("awk '{if($1==\"pzzl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
-  pxyl = commands.getoutput("awk '{if($1==\"pxyl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
-  pxzl = commands.getoutput("awk '{if($1==\"pxzl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
-  pyzl = commands.getoutput("awk '{if($1==\"pyzl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
-  pxxp = commands.getoutput("awk '{if($1==\"#S\"){print $2}}' config")
-  pyyp = commands.getoutput("awk '{if($1==\"#S\"){print $3}}' config")
-  pzzp = commands.getoutput("awk '{if($1==\"#S\"){print $4}}' config")
-  pxyp = commands.getoutput("awk '{if($1==\"#S\"){print $5}}' config")
-  pxzp = commands.getoutput("awk '{if($1==\"#S\"){print $6}}' config")
-  pyzp = commands.getoutput("awk '{if($1==\"#S\"){print $7}}' config")
-  diffpxx = (float(pxxl) - float(pxxp))/(float(pxxp)+0.000000101)*100.0/6.0
-  diffpyy = (float(pyyl) - float(pyyp))/(float(pyyp)+0.000000101)*100.0/6.0
-  diffpzz = (float(pzzl) - float(pzzp))/(float(pzzp)+0.000000101)*100.0/6.0
-  diffpxy = (float(pxyl) - float(pxyp))/(float(pxyp)+0.000000101)*100.0/6.0
-  diffpxz = (float(pxzl) - float(pxzp))/(float(pxzp)+0.000000101)*100.0/6.0
-  diffpyz = (float(pyzl) - float(pyzp))/(float(pyzp)+0.000000101)*100.0/6.0
-  diffp = abs(diffpxx) + abs(diffpyy) + abs(diffpzz) + abs(diffpxy) + abs(diffpxz) + abs(diffpyz)
-  print "lammps: "+str(pxxl)+", "+str(pyyl)+", "+str(pzzl)+", "+str(pxyl)+", "+str(pxzl)+", "+str(pyzl)+" [eV/A^3]"
-  print "pwscf:  "+str(pxxp)+", "+str(pyyp)+", "+str(pzzp)+", "+str(pxyp)+", "+str(pxzp)+", "+str(pyzp)+" [eV/A^3]"
-  print "P diff (%): "+str(diffp)
-  print "---------------"
+    # stress = pressure
+    pxxl = commands.getoutput("awk '{if($1==\"pxxl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
+    pyyl = commands.getoutput("awk '{if($1==\"pyyl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
+    pzzl = commands.getoutput("awk '{if($1==\"pzzl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
+    pxyl = commands.getoutput("awk '{if($1==\"pxyl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
+    pxzl = commands.getoutput("awk '{if($1==\"pxzl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
+    pyzl = commands.getoutput("awk '{if($1==\"pyzl\"){printf \"%10.8f\",$3*7.4028083e-11}}' log.lammps")
+    pxxp = commands.getoutput("awk '{if($1==\"#S\"){print $2}}' config_"+str(t)+"K")
+    pyyp = commands.getoutput("awk '{if($1==\"#S\"){print $3}}' config_"+str(t)+"K")
+    pzzp = commands.getoutput("awk '{if($1==\"#S\"){print $4}}' config_"+str(t)+"K")
+    pxyp = commands.getoutput("awk '{if($1==\"#S\"){print $5}}' config_"+str(t)+"K")
+    pxzp = commands.getoutput("awk '{if($1==\"#S\"){print $6}}' config_"+str(t)+"K")
+    pyzp = commands.getoutput("awk '{if($1==\"#S\"){print $7}}' config_"+str(t)+"K")
+    diffpxx = (float(pxxl) - float(pxxp))/(float(pxxp)+0.000000101)*100.0/6.0
+    diffpyy = (float(pyyl) - float(pyyp))/(float(pyyp)+0.000000101)*100.0/6.0
+    diffpzz = (float(pzzl) - float(pzzp))/(float(pzzp)+0.000000101)*100.0/6.0
+    diffpxy = (float(pxyl) - float(pxyp))/(float(pxyp)+0.000000101)*100.0/6.0
+    diffpxz = (float(pxzl) - float(pxzp))/(float(pxzp)+0.000000101)*100.0/6.0
+    diffpyz = (float(pyzl) - float(pyzp))/(float(pyzp)+0.000000101)*100.0/6.0
+    diffp = abs(diffpxx) + abs(diffpyy) + abs(diffpzz) + abs(diffpxy) + abs(diffpxz) + abs(diffpyz)
+    print "lammps: "+str(pxxl)+", "+str(pyyl)+", "+str(pzzl)+", "+str(pxyl)+", "+str(pxzl)+", "+str(pyzl)+" [eV/A^3]"
+    print "PWscf:  "+str(pxxp)+", "+str(pyyp)+", "+str(pzzp)+", "+str(pxyp)+", "+str(pxzp)+", "+str(pyzp)+" [eV/A^3]"
+    print "P diff (%): "+str(diffp)
+    print "---------------"
 
-  lammps_get_data = "grep \"Total Energy\" log.lammps | tail -1 | awk '{printf \"%-20.10f\",$4}'"
-  lmpe = commands.getoutput(lammps_get_data)
+    # force
+    difffx = 0.0
+    difffy = 0.0
+    difffz = 0.0
+    difff  = 0.0
+    for i in range(int(natom)):
+      fxl[i] = commands.getoutput("awk '{if(NR==10+"+str(i)+"){printf \"%10.8f\",$7}}' trajectory.lammpstrj")
+      fyl[i] = commands.getoutput("awk '{if(NR==10+"+str(i)+"){printf \"%10.8f\",$8}}' trajectory.lammpstrj")
+      fzl[i] = commands.getoutput("awk '{if(NR==10+"+str(i)+"){printf \"%10.8f\",$9}}' trajectory.lammpstrj")
+      fxp[i] = commands.getoutput("awk '{if(NR==11+"+str(i)+"){print $5}}' config_"+str(t)+"K")
+      fyp[i] = commands.getoutput("awk '{if(NR==11+"+str(i)+"){print $6}}' config_"+str(t)+"K")
+      fzp[i] = commands.getoutput("awk '{if(NR==11+"+str(i)+"){print $7}}' config_"+str(t)+"K")
+      difffx = (float(fxl[i]) - float(fxp[i]))/(float(fxp[i])+0.000000101)*100.0/3.0/float(natom)
+      difffy = (float(fyl[i]) - float(fyp[i]))/(float(fyp[i])+0.000000101)*100.0/3.0/float(natom)
+      difffz = (float(fzl[i]) - float(fzp[i]))/(float(fzp[i])+0.000000101)*100.0/3.0/float(natom)
+      difff  = difff + abs(difffx) + abs(difffy) + abs(difffz)
+    print "lammps: "+str(fxl[0])+" : "+str(fyl[0])+" : "+str(fzl[0])+" [eV/A]"
+    print "PWscf: "+str(fxp[0])+" : "+str(fyp[0])+" : "+str(fzp[0])+" [eV/A]"
+    print "force diff (%): "+str(difff)
+    print "---------------"
 
-  pwe = commands.getoutput("awk '{if($1==\"#E\"){print $2}}' config")
-  pwe = float(pwe) * float(natom)
+    lammps_get_data = "grep \"Total Energy\" log.lammps | tail -1 | awk '{printf \"%-20.10f\",$4}'"
+    lmpe = commands.getoutput(lammps_get_data)
 
-  print "lammps: "+str(lmpe)+" [eV]"
+    pwe = commands.getoutput("awk '{if($1==\"#E\"){print $2}}' config_"+str(t)+"K")
+    pwe = float(pwe) * float(natom)
 
-  print "PWscf:  "+str(pwe)+" [eV]"
+    print "lammps: "+str(lmpe)+" [eV]"
 
-  diffe = float(pwe) - float(lmpe)
-  print "diff: "+str(diffe)+" [eV]"
-  diffea = float(diffe)/float(natom)
-  print "diff/atom: "+str(diffea)+" [eV/atom]"
-  commands.getoutput("echo "+str(count)+" "+str(diffe)+" >> energy.dat")
+    print "PWscf:  "+str(pwe)+" [eV]"
 
-  rhoin  = float(x2)*float(x21)
-  rhoout = float(x2)*1.15
-  print "---------------"
-  print "F boundary 1, rho: "+str(rhoin)
-  print "F boundary 2, rho: "+str(x2)
-  print "F boundary 3, rho: "+str(rhoout)
-  commands.getoutput("cp "+satom+"_Zhou04.eam.alloy"+" Xx_Zhou04.eam.alloy")
-  commands.getoutput("./plot")
-  rhoin1  = commands.getoutput("cat F.plt | awk '{if($1<"+str(rhoin)+"){print $2}}' | tail -2 | head -1")
-  rhoin2  = commands.getoutput("cat F.plt | awk '{if($1>"+str(rhoin)+"){print $2}}' | head -2 | tail -1")
-  rhoe1   = commands.getoutput("cat F.plt | awk '{if($1<"+str(x2)+"){print $2}}' | tail -2 | head -1")
-  rhoe2   = commands.getoutput("cat F.plt | awk '{if($1>"+str(x2)+"){print $2}}' | head -2 | tail -1")
-  rhoout1 = commands.getoutput("cat F.plt | awk '{if($1<"+str(rhoout)+"){print $2}}' | tail -2 | head -1")
-  rhoout2 = commands.getoutput("cat F.plt | awk '{if($1>"+str(rhoout)+"){print $2}}' | head -2 | tail -1")
-  print "F near boundary 1, F: "+str(rhoin1)+" | "+str(rhoin2)+" | diff "+str(float(rhoin1) - float(rhoin2))
-  print "F near boundary 2, F: "+str(rhoe1)+" | "+str(rhoe2)+" | diff "+str(float(rhoe1) - float(rhoe2))
-  print "F near boundary 3, F: "+str(rhoout1)+" | "+str(rhoout2)+" | diff "+str(float(rhoout1) - float(rhoout2))
+    diffe = float(pwe) - float(lmpe)
+    print "diff: "+str(diffe)+" [eV]"
+    diffea = float(diffe)/float(natom)
+    print "diff/atom: "+str(diffea)+" [eV/atom]"
+    commands.getoutput("echo "+str(count)+" "+str(diffe)+" >> energy.dat")
+
+    tdiffea = tdiffea + float(diffea)
+    tdiffp  = tdiffp  + float(diffp)
+    tdifff  = tdifff  + float(difff)
+  
+  diffb  = commands.getoutput("cat diff.dat")
+  print "F boundary, diff: "+str(diffb)
+  diffb  = commands.getoutput("cat diff.dat")
+
   print "---------------"
   
-  y = 0.001/(abs(diffea)**2 + 1000*abs(float(rhoin1) - float(rhoin2))**2 + 1000*abs(float(rhoe1) - float(rhoe2))**2 + 1000*abs(float(rhoout1) - float(rhoout2))**2 + 0.0000002*abs(diffp)**2)
+  y = 0.001/(float(tdiffea)**2 + 1000*float(diffb)**2 + 0.0000002*abs(tdiffp)**2 + 0.0000010*abs(tdifff)**2)
 
   print "Evaluate: ", y
   print "------------------------"
