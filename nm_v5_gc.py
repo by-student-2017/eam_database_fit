cc1b61070054c347195d51967dad30b7f8ca7229
by-student-2017 <studentsctest@gmail.com>
2020-10-12 07:40:42 +0900

    debian 10.0

--- nm_v5_gc.py
+++ nm_v5_gc.py
@@ -137,7 +137,7 @@ def f(x):
   for t in temp:
     print "---------------"
     print "Temperature: "+str(t)+" [K]"
-    if count > 5000 or count % int(3500*2.718**(-count/3500)+1) == 1: 
+    if count > 5000 or count % int(4000*2.718**(-count/4000)+1) == 1: 
       commands.getoutput("mv data.in_"+str(t)+"K data.in")
       natom = commands.getoutput("awk '{if($2==\"atoms\"){print $1}}' data.in")
       commands.getoutput(lammps_adress+" < in.lmp_"+str(t)+"K")
